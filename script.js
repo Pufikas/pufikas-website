23bc31b25c52b4a5137e3510943239366945b290
Pufikas <dominykas.guzys@gmail.com>
2025-08-20 21:28:59 +0300

    audio wip 2

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
+// audio player stuff
 let counter = 0;
 const songs = [
-    "0.mp3",
-    "1.mp3",
-    "2.mp3",
-];
-const audio = document.getElementById("audioplayer");
+    {
+        "song": "0.mp3",
+        "title": "1",
+        "cover": "internet_overdose.jpg" 
+    },
+    {
+        "song": "1.mp3",
+        "title": "2",
+        "cover": "internet_overdose.jpg" 
+    },
+]
+const musicPath = `./assets/music/`;
+const audio = document.getElementById("audio-player");
 const audioDuration = document.getElementById("audio-duration");
 const audioTotal = document.getElementById("audio-total");
 const line = document.getElementById("audio-progress-line");
+const audioProgress = document.getElementById("audio-progress-container");
+const audioPlay = document.getElementById("audio-pause-play");
+const audioNext = document.getElementById("audio-play-next");
+const audioCover = document.getElementById("audio-cover");
+const audioVolumeLine = document.getElementById("audio-volume-line");
+const audioVolume = document.getElementById("audio-volume");
+// end of audio player stuff
 
 function timer() {
     let res = document.getElementById("time");
@@ -39,32 +55,25 @@ function showPanel(option) {
     })
 }
 
-function loadStuff() {
-    const audioPlayer = document.getElementById
-}
-
-let music = [
-    ""
-]
 
 function playAudio() {
-    const btn = document.getElementById("audio-pause-play");
-
     if (audio.paused) {
         audio.play();
-        btn.innerText = "▶"
+        audioPlay.innerText = "❚❚"
     } else {
         audio.pause();
-        btn.innerText = "❚❚"
+        audioPlay.innerText = "▶"
     }
 
 }
 
 function audioPlayNext() {
     counter = (counter + 1) % songs.length;
-    audio.src = `./assets/music/${songs[counter]}`;
-    audio.volume = 0.2;
-    audio.play();
+    audio.src = `${musicPath}${songs[counter].song}`;
+    
+    console.log(songs)
+    console.log(musicPath, songs[counter].song)
+    playAudio();
 
     console.log(counter)
 }
@@ -75,14 +84,39 @@ function formatTime(sec) {
     return `${min}:${s.toString().padStart(2, '0')}`;
 }
 
+function seekIntoMusic(e) {
+    let percent = e.offsetX / audioProgress.offsetWidth;
+    audio.currentTime = percent * audio.duration;
+    line.style.width = percent * 100 + "%";
+}
+
+function changeVolume(e) {
+    const percent = e.offsetX / audioVolume.offsetWidth;
+    audio.volume = percent;
+    audioVolumeLine.style.width = `${percent * 100}%`;
+    console.log(percent)
+}
+
+audioVolume.addEventListener("click", changeVolume);
+
 audio.addEventListener("loadedmetadata", () => {
     audioTotal.textContent = formatTime(audio.duration);
+    audioCover.innerHTML = `<img src=${musicPath}${songs[counter].cover} style="width: 128px;" class="center">`;
 });
 
 audio.addEventListener("timeupdate", () => {
     audioDuration.textContent = formatTime(audio.currentTime);
     const progress = (audio.currentTime / audio.duration) * 100;
     line.style.width = `${progress}%`;
+
+    if (line.style.width == "100%") {
+        audio.pause();
+    }
 });
+
+audioPlay.addEventListener("click", playAudio);
+audioNext.addEventListener("click", audioPlayNext);
+audioProgress.addEventListener("click", seekIntoMusic.bind(this));
+
 
 setInterval(timer, 1000);
