accab4af6cc5bc24809e3419da545d3787ae5748
Pufikas <pufikas@proton.me>
2026-01-07 22:16:42 +0200

    more polish on themes

--- src/scripts/themes.js
+++ src/scripts/themes.js
@@ -1,18 +1,25 @@
-let switches = document.querySelectorAll(".switch");
-let container = document.getElementById("theme-container");
-let button = document.getElementById("theme-switch");
-let themeInner = document.getElementById("theme-inner");
-let currThemeText = document.getElementById("current-theme");
-const themes = ["miku", "teto", "neru"]
-let currTheme = "";
-let i = 0;
+// let switches = document.querySelectorAll(".switch");
+// let container = document.getElementById("theme-container");
+// let themeInner = document.getElementById("theme-inner");
 
-button.addEventListener("click", () => {
-    currTheme = themes[i % themes.length];
-    currThemeText.innerText = currTheme;
 
-    document.documentElement.setAttribute("data-theme", currTheme);
+const themes = ["miku", "teto", "neru"];
 
-    i++;
-})
+// syncs the rotation to next theme based on current theme, and also need to parse cuz I stringified it earlier...
+let i = themes.indexOf(JSON.parse(localStorage.getItem("theme"))); 
+if (i === -1) i = 0; // if something corrupts or smth
 
+function updateThemeStuff(index) {
+    const theme = themes[index];
+    document.getElementById("current-theme").innerText = theme;
+    document.documentElement.dataset.theme = theme;
+    localStorage.setItem("theme", JSON.stringify(theme)); // need to stringify (even though it's already a string) so it becomes a valid json
+    settings.theme = theme;
+}
+
+document.getElementById("theme-switch").addEventListener("click", () => {
+    i = (i + 1) % themes.length;
+    updateThemeStuff(i);
+});
+
+updateThemeStuff(i);
